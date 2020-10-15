a2dba98afc872647999f08499929600da6275215
René Preuß <rene@preuss.io>
2020-10-15 15:31:25 +0200

    Add new permissions

--- src/Permission/Permission.php
+++ src/Permission/Permission.php
@@ -5,7 +5,11 @@ namespace Own3d\Id\Permission;
 class Permission
 {
     // 1, 2, 4, 8
-    public const ADMINISTRATOR          = 0x00000001;
-    public const CDN          = 0x00000002;
-    public const SHOP          = 0x00000003;
+    public const ADMINISTRATOR   = 1 << 0;
+    public const MANAGE_CDN      = 1 << 1;
+    public const MANAGE_SHOP     = 1 << 2;
+    public const VIEW_LOGS       = 1 << 3;
+    public const VIEW_KPIS       = 1 << 4;
+    public const MANAGE_ROLES    = 1 << 5;
+    public const MANAGE_USERS    = 1 << 6;
 }
