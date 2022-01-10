2102a2b3315ea7fc6e163e6a9a8ad36a71d28e8d
René Preuß <rene@preuss.io>
2022-01-10 14:52:56 +0100

    Update Permission.php

--- src/Permission/Permission.php
+++ src/Permission/Permission.php
@@ -4,7 +4,10 @@ namespace Own3d\Id\Permission;
 
 class Permission
 {
+    // administration
     public const ADMINISTRATOR = 1 << 0;
+
+    // features
     public const MANAGE_CDN = 1 << 1;
     public const MANAGE_SHOP = 1 << 2;
     public const VIEW_LOGS = 1 << 3;
@@ -13,5 +16,13 @@ class Permission
     public const MANAGE_USERS = 1 << 6;
     public const VIEW_ADDRESSES = 1 << 7;
     public const MANAGE_FULFILLMENT = 1 << 8;
+
+    // functions
     public const STAFF = 1 << 9;
+    public const MARKETING = 1 << 10;
+    public const SUPPORT = 1 << 11;
+
+    // aliases
+    public const MANAGER = self::ADMINISTRATOR;
+    public const SHOP_ADMIN = self::MANAGE_SHOP;
 }
